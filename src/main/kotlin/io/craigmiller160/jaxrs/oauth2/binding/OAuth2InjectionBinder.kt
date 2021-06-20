03341a917214dd3dc93d1b8cdd8b490bbcce885f
Craig Miller <craigmiller160@gmail.com>
2021-06-20 10:45:51 -0400

    Fixed multiple instantiation of config class

--- src/main/kotlin/io/craigmiller160/jaxrs/oauth2/binding/OAuth2InjectionBinder.kt
+++ src/main/kotlin/io/craigmiller160/jaxrs/oauth2/binding/OAuth2InjectionBinder.kt
@@ -3,6 +3,7 @@ package io.craigmiller160.jaxrs.oauth2.binding
 import io.craigmiller160.jaxrs.oauth2.config.OAuth2ConfigImpl
 import io.craigmiller160.oauth2.config.OAuth2Config
 import org.glassfish.hk2.utilities.binding.AbstractBinder
+import javax.inject.Singleton
 import javax.ws.rs.ext.Provider
 
 @Provider
@@ -10,5 +11,6 @@ class OAuth2InjectionBinder : AbstractBinder() {
     override fun configure() {
         bind(OAuth2ConfigImpl::class.java)
                 .to(OAuth2Config::class.java)
+                .`in`(Singleton::class.java)
     }
 }
