1328fa8bd9593399231d7e304a157096416eaba6
Craig Miller <craigmiller160@gmail.com>
2021-07-16 14:52:18 -0400

    Implemented first method

--- src/main/kotlin/io/craigmiller160/jaxrs/oauth2/domain/repository/JdbcRefreshTokenRepository.kt
+++ src/main/kotlin/io/craigmiller160/jaxrs/oauth2/domain/repository/JdbcRefreshTokenRepository.kt
@@ -1,14 +1,24 @@
 package io.craigmiller160.jaxrs.oauth2.domain.repository
 
+import io.craigmiller160.jaxrs.oauth2.domain.SqlConnectionProvider
 import io.craigmiller160.oauth2.domain.entity.AppRefreshToken
 import io.craigmiller160.oauth2.domain.repository.AppRefreshTokenRepository
 
-class JdbcRefreshTokenRepository : AppRefreshTokenRepository {
-
-    // TODO need datasource from DI
+class JdbcRefreshTokenRepository(
+    private val sqlConnectionProvider: SqlConnectionProvider
+) : AppRefreshTokenRepository {
+    private val deleteById = """
+        DELETE FROM app_refresh_tokens
+        WHERE id = ?
+    """.trimIndent()
 
     override fun deleteById(id: Long) {
-        TODO("Not yet implemented")
+        sqlConnectionProvider.provide().use { conn ->
+            conn.prepareStatement(deleteById).use { stmt ->
+                stmt.setLong(1, id)
+                stmt.executeUpdate()
+            }
+        }
     }
 
     override fun findByTokenId(tokenId: String): AppRefreshToken? {
