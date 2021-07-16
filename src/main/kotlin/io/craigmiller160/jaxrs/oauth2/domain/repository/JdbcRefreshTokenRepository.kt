4b084c97578e44ae959cf025c88ccf445ab33bba
Craig Miller <craigmiller160@gmail.com>
2021-07-16 14:55:02 -0400

    Implemented another method

--- src/main/kotlin/io/craigmiller160/jaxrs/oauth2/domain/repository/JdbcRefreshTokenRepository.kt
+++ src/main/kotlin/io/craigmiller160/jaxrs/oauth2/domain/repository/JdbcRefreshTokenRepository.kt
@@ -1,8 +1,10 @@
 package io.craigmiller160.jaxrs.oauth2.domain.repository
 
 import io.craigmiller160.jaxrs.oauth2.domain.SqlConnectionProvider
+import io.craigmiller160.jaxrs.oauth2.domain.entity.JdbcAppRefreshToken
 import io.craigmiller160.oauth2.domain.entity.AppRefreshToken
 import io.craigmiller160.oauth2.domain.repository.AppRefreshTokenRepository
+import java.sql.ResultSet
 
 class JdbcRefreshTokenRepository(
     private val sqlConnectionProvider: SqlConnectionProvider
@@ -10,6 +12,11 @@ class JdbcRefreshTokenRepository(
     private val deleteById = """
         DELETE FROM app_refresh_tokens
         WHERE id = ?
+    """.trimIndent()
+    private val selectByTokenId = """
+        SELECT *
+        FROM app_refresh_tokens
+        WHERE token_id = ?
     """.trimIndent()
 
     override fun deleteById(id: Long) {
@@ -21,8 +28,26 @@ class JdbcRefreshTokenRepository(
         }
     }
 
+    private fun resultSetToEntity(rs: ResultSet): AppRefreshToken =
+            JdbcAppRefreshToken(
+                    id = rs.getLong("id"),
+                    tokenId = rs.getString("token_id"),
+                    refreshToken = rs.getString("refresh_token")
+            )
+
     override fun findByTokenId(tokenId: String): AppRefreshToken? {
-        TODO("Not yet implemented")
+        return sqlConnectionProvider.provide().use { conn ->
+            conn.prepareStatement(selectByTokenId).use { stmt ->
+                stmt.setString(1, tokenId)
+                stmt.executeQuery().use { rs ->
+                    if (rs.next()) {
+                        resultSetToEntity(rs)
+                    } else {
+                        null
+                    }
+                }
+            }
+        }
     }
 
     override fun removeByTokenId(tokenId: String) {
