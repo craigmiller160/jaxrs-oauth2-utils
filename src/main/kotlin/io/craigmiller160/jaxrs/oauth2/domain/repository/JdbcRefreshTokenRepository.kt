4d1378d7a552e56f65d26400638ced6dbf6c5565
Craig Miller <craigmiller160@gmail.com>
2021-07-16 14:23:52 -0400

    Stubbed out repository methods and tests

---
+++ src/main/kotlin/io/craigmiller160/jaxrs/oauth2/domain/repository/JdbcRefreshTokenRepository.kt
@@ -0,0 +1,22 @@
+package io.craigmiller160.jaxrs.oauth2.domain.repository
+
+import io.craigmiller160.oauth2.domain.entity.AppRefreshToken
+import io.craigmiller160.oauth2.domain.repository.AppRefreshTokenRepository
+
+class JdbcRefreshTokenRepository : AppRefreshTokenRepository {
+    override fun deleteById(id: Long) {
+        TODO("Not yet implemented")
+    }
+
+    override fun findByTokenId(tokenId: String): AppRefreshToken? {
+        TODO("Not yet implemented")
+    }
+
+    override fun removeByTokenId(tokenId: String) {
+        TODO("Not yet implemented")
+    }
+
+    override fun save(token: AppRefreshToken): AppRefreshToken {
+        TODO("Not yet implemented")
+    }
+}
---
+++ src/test/java/io/craigmiller160/jaxrs/oauth2/domain/repository/JdbcRefreshTokenRepositoryTest.kt
@@ -0,0 +1,25 @@
+package io.craigmiller160.jaxrs.oauth2.domain.repository
+
+class JdbcRefreshTokenRepositoryTest {
+
+    fun `deleteById()`() {
+        TODO("Finish this")
+    }
+
+    fun `removeByTokenId()`() {
+        TODO("Finish this")
+    }
+
+    fun `findByTokenId()`() {
+        TODO("Finish this")
+    }
+
+    fun `save() new token`() {
+        TODO("Finish this")
+    }
+
+    fun `save() existing token`() {
+        TODO("Finish this")
+    }
+
+}
