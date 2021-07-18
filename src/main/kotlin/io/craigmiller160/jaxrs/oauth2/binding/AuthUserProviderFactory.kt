8ebade42dd68c05b0e24cbddd42075d6ba0adb44
Craig Miller <craigmiller160@gmail.com>
2021-07-18 12:16:47 -0400

    Fixed broken request context

--- src/main/kotlin/io/craigmiller160/jaxrs/oauth2/binding/AuthUserProviderFactory.kt
+++ src/main/kotlin/io/craigmiller160/jaxrs/oauth2/binding/AuthUserProviderFactory.kt
@@ -4,16 +4,17 @@ import io.craigmiller160.jaxrs.oauth2.security.AuthConstants
 import io.craigmiller160.oauth2.security.AuthenticatedUser
 import io.craigmiller160.oauth2.service.AuthUserProvider
 import org.glassfish.hk2.api.Factory
+import javax.inject.Inject
+import javax.inject.Provider
 import javax.ws.rs.container.ContainerRequestContext
 import javax.ws.rs.core.Context
 
-class AuthUserProviderFactory : Factory<AuthUserProvider> {
-
-    @Context
-    private lateinit var reqContext: ContainerRequestContext
+class AuthUserProviderFactory @Inject constructor(
+        private val reqContextProvider: Provider<ContainerRequestContext>
+) : Factory<AuthUserProvider> {
 
     override fun provide(): AuthUserProvider = AuthUserProvider {
-        reqContext.getProperty(AuthConstants.AUTHENTICATED_USER) as AuthenticatedUser
+        reqContextProvider.get().getProperty(AuthConstants.AUTHENTICATED_USER) as AuthenticatedUser
     }
 
     override fun dispose(provider: AuthUserProvider?) {}
