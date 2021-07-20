1045d2b83dbd25fe8896910fc9614aaa7a39d81f
Craig Miller <craigmiller160@gmail.com>
2021-07-20 16:13:34 -0400

    Moved all paths to constants

--- src/main/kotlin/io/craigmiller160/jaxrs/oauth2/resource/OAuth2Resource.kt
+++ src/main/kotlin/io/craigmiller160/jaxrs/oauth2/resource/OAuth2Resource.kt
@@ -1,6 +1,7 @@
 package io.craigmiller160.jaxrs.oauth2.resource
 
 import io.craigmiller160.oauth2.dto.AuthCodeLoginDto
+import io.craigmiller160.oauth2.resource.ResourceConstants
 import io.craigmiller160.oauth2.service.AuthCodeService
 import io.craigmiller160.oauth2.service.OAuth2Service
 import javax.inject.Inject
@@ -12,7 +13,7 @@ import javax.ws.rs.QueryParam
 import javax.ws.rs.core.Context
 import javax.ws.rs.core.Response
 
-@Path("/oauth")
+@Path(ResourceConstants.ROOT_PATH)
 class OAuth2Resource @Inject constructor(
         private val authCodeService: AuthCodeService,
         private val oAuthService: OAuth2Service
@@ -21,7 +22,7 @@ class OAuth2Resource @Inject constructor(
     lateinit var req: HttpServletRequest
 
     @POST
-    @Path("/authcode/login")
+    @Path(ResourceConstants.AUTHCODE_LOGIN_PATH)
     fun login(): Response {
         val authCodeLoginUrl = authCodeService.prepareAuthCodeLogin(req)
         return Response
@@ -30,7 +31,7 @@ class OAuth2Resource @Inject constructor(
     }
 
     @GET
-    @Path("/authcode/code")
+    @Path(ResourceConstants.AUTHCODE_CODE_PATH)
     fun code(@QueryParam("code") code: String, @QueryParam("state") state: String): Response {
         val result = authCodeService.code(req, code, state)
         return Response
@@ -41,7 +42,7 @@ class OAuth2Resource @Inject constructor(
     }
 
     @GET
-    @Path("/logout")
+    @Path(ResourceConstants.LOGOUT_PATH)
     fun logout(): Response {
         val cookie = oAuthService.logout()
         return Response
@@ -51,7 +52,7 @@ class OAuth2Resource @Inject constructor(
     }
 
     @GET
-    @Path("/user")
+    @Path(ResourceConstants.AUTH_USER_PATH)
     fun getAuthenticatedUser(): Response {
         val authUser = oAuthService.getAuthenticatedUser()
         return Response
