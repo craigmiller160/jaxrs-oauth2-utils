09a734570deb47da927a76280134c0d001e383bf
Craig Miller <craigmiller160@gmail.com>
2021-07-20 15:49:35 -0400

    Finalized binding

--- src/main/kotlin/io/craigmiller160/jaxrs/oauth2/binding/OAuth2InjectionBinder.kt
+++ src/main/kotlin/io/craigmiller160/jaxrs/oauth2/binding/OAuth2InjectionBinder.kt
@@ -3,6 +3,7 @@ package io.craigmiller160.jaxrs.oauth2.binding
 import io.craigmiller160.jaxrs.oauth2.client.AuthServerClientInjectImpl
 import io.craigmiller160.jaxrs.oauth2.config.OAuth2ConfigImpl
 import io.craigmiller160.jaxrs.oauth2.domain.AppRefreshTokenRepositoryInjectImpl
+import io.craigmiller160.jaxrs.oauth2.service.AuthCodeServiceInjectImpl
 import io.craigmiller160.jaxrs.oauth2.service.OAuth2ServiceInjectImpl
 import io.craigmiller160.jaxrs.oauth2.service.RefreshTokenServiceInjectImpl
 import io.craigmiller160.oauth2.client.AuthServerClient
@@ -10,6 +11,7 @@ import io.craigmiller160.oauth2.config.OAuth2Config
 import io.craigmiller160.oauth2.domain.repository.AppRefreshTokenRepository
 import io.craigmiller160.oauth2.provider.AuthUserProvider
 import io.craigmiller160.oauth2.security.CookieCreator
+import io.craigmiller160.oauth2.service.AuthCodeService
 import io.craigmiller160.oauth2.service.OAuth2Service
 import io.craigmiller160.oauth2.service.RefreshTokenService
 import org.glassfish.hk2.utilities.binding.AbstractBinder
@@ -35,6 +37,9 @@ class OAuth2InjectionBinder : AbstractBinder() {
                 .`in`(Singleton::class.java)
         bind(RefreshTokenServiceInjectImpl::class.java)
                 .to(RefreshTokenService::class.java)
+                .`in`(Singleton::class.java)
+        bind(AuthCodeServiceInjectImpl::class.java)
+                .to(AuthCodeService::class.java)
                 .`in`(Singleton::class.java)
 
         bindFactory(AuthUserProviderFactory::class.java)
