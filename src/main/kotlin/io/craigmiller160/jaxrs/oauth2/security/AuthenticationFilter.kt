d6222a7a714729689c321da9fb5e277cb6116556
Craig Miller <craigmiller160@gmail.com>
2021-07-26 16:31:45 -0400

    Implemented a lot of AuthenticationFilter

--- src/main/kotlin/io/craigmiller160/jaxrs/oauth2/security/AuthenticationFilter.kt
+++ src/main/kotlin/io/craigmiller160/jaxrs/oauth2/security/AuthenticationFilter.kt
@@ -1,7 +1,11 @@
 package io.craigmiller160.jaxrs.oauth2.security
 
+import com.nimbusds.jwt.JWTClaimsSet
+import io.craigmiller160.oauth2.security.AuthenticatedUser
 import io.craigmiller160.oauth2.security.AuthenticationFilterService
 import io.craigmiller160.oauth2.security.RequestWrapper
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import javax.inject.Inject
 import javax.ws.rs.container.ContainerRequestContext
 import javax.ws.rs.container.ContainerRequestFilter
@@ -15,25 +19,39 @@ class AuthenticationFilter @Inject constructor(
         private val authenticationFilterService: AuthenticationFilterService
 ) : ContainerRequestFilter, ContainerResponseFilter {
 
-    override fun filter(req: ContainerRequestContext?) {
+    private val logger: Logger = LoggerFactory.getLogger(javaClass)
+
+    override fun filter(req: ContainerRequestContext) {
         authenticationFilterService.authenticateRequest(RequestWrapper(
-                requestUri = req?.uriInfo?.path!!,
-                getCookieValue = { cookieName ->
-
-                },
-                getHeaderValue = { headerName ->
-
-                },
+                requestUri = req.uriInfo.path,
+                getCookieValue = { cookieName -> req.cookies[cookieName]?.value },
+                getHeaderValue = { headerName -> req.getHeaderString(headerName) },
                 setAuthentication = { claims ->
-
+                    val authentication = createAuthenticatedUser(claims)
+                    req.setProperty(AuthConstants.AUTHENTICATED_USER, authentication)
                 },
                 setNewTokenCookie = { cookie ->
-
+                    req.setProperty(AuthConstants.NEW_TOKEN_FOR_COOKIE, cookie)
                 }
         ))
+                .onFailure { ex ->
+                    logger.error("Token validation failed: ${ex.message}")
+                    logger.debug("", ex)
+                    // TODO need to abort with 401
+                }
     }
 
-    override fun filter(req: ContainerRequestContext?, res: ContainerResponseContext?) {
-        TODO("Not yet implemented")
+    private fun createAuthenticatedUser(claims: JWTClaimsSet): AuthenticatedUser =
+            AuthenticatedUserImpl(
+                    userName = claims.subject,
+                    roles = claims.getStringListClaim("roles"),
+                    firstName = claims.getStringClaim("firstName"),
+                    lastName = claims.getStringClaim("lastName"),
+                    tokenId = claims.jwtid
+            )
+
+    override fun filter(req: ContainerRequestContext, res: ContainerResponseContext) {
+        req.getProperty(AuthConstants.NEW_TOKEN_FOR_COOKIE)
+                ?.let { cookie -> res.headers.add("Set-Cookie", cookie) }
     }
 }
