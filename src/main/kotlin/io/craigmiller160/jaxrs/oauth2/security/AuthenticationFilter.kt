217f9d69789433f039c7991ef876fe7b93cd94b3
Craig Miller <craigmiller160@gmail.com>
2021-07-26 16:23:00 -0400

    Implementing part of AuthenticationFilter

--- src/main/kotlin/io/craigmiller160/jaxrs/oauth2/security/AuthenticationFilter.kt
+++ src/main/kotlin/io/craigmiller160/jaxrs/oauth2/security/AuthenticationFilter.kt
@@ -1,16 +1,35 @@
 package io.craigmiller160.jaxrs.oauth2.security
 
 import io.craigmiller160.oauth2.security.AuthenticationFilterService
+import io.craigmiller160.oauth2.security.RequestWrapper
+import org.jvnet.hk2.annotations.Service
+import javax.inject.Inject
 import javax.ws.rs.container.ContainerRequestContext
 import javax.ws.rs.container.ContainerRequestFilter
 import javax.ws.rs.container.ContainerResponseContext
 import javax.ws.rs.container.ContainerResponseFilter
 
-class AuthenticationFilter(
+@Service
+class AuthenticationFilter @Inject constructor(
         private val authenticationFilterService: AuthenticationFilterService
 ) : ContainerRequestFilter, ContainerResponseFilter {
+
     override fun filter(req: ContainerRequestContext?) {
-        TODO("Not yet implemented")
+        authenticationFilterService.authenticateRequest(RequestWrapper(
+                requestUri = req?.uriInfo?.path!!,
+                getCookieValue = { cookieName ->
+
+                },
+                getHeaderValue = { headerName ->
+
+                },
+                setAuthentication = { claims ->
+
+                },
+                setNewTokenCookie = { cookie ->
+
+                }
+        ))
     }
 
     override fun filter(req: ContainerRequestContext?, res: ContainerResponseContext?) {
