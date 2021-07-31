140d74f10eea4be16f3a407bd4bc67241abad578
Craig Miller <craigmiller160@gmail.com>
2021-07-31 09:22:45 -0400

    Added exception mapper

---
+++ src/main/kotlin/io/craigmiller160/jaxrs/oauth2/dto/ErrorDto.kt
@@ -0,0 +1,6 @@
+package io.craigmiller160.jaxrs.oauth2.dto
+
+data class ErrorDto(
+        val message: String,
+        val status: Int
+)
--- src/main/kotlin/io/craigmiller160/jaxrs/oauth2/exception/OAuth2ExceptionMapper.kt
+++ src/main/kotlin/io/craigmiller160/jaxrs/oauth2/exception/OAuth2ExceptionMapper.kt
@@ -1,13 +1,36 @@
 package io.craigmiller160.jaxrs.oauth2.exception
 
+import io.craigmiller160.jaxrs.oauth2.dto.ErrorDto
 import io.craigmiller160.oauth2.exception.OAuth2Exception
+import javax.ws.rs.core.MediaType
 import javax.ws.rs.core.Response
 import javax.ws.rs.ext.ExceptionMapper
 import javax.ws.rs.ext.Provider
 
 @Provider
 class OAuth2ExceptionMapper : ExceptionMapper<OAuth2Exception> {
-    override fun toResponse(ex: OAuth2Exception?): Response {
-        TODO("Not yet implemented")
+    override fun toResponse(ex: OAuth2Exception): Response {
+        val error = ErrorDto(
+                status = ex.statusCode,
+                message = getMessage(ex)
+        )
+
+        return Response
+                .status(ex.statusCode)
+                .entity(error)
+                .encoding(MediaType.APPLICATION_JSON)
+                .build()
+    }
+
+    private fun getMessage(ex: OAuth2Exception): String {
+        var message = ex.message ?: ""
+        var cause = ex.cause
+        while (cause != null) {
+            message = cause.message
+                    ?.let { "$message - $it" }
+                    ?: message
+            cause = cause.cause
+        }
+        return message
     }
 }
